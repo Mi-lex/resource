5c405f2446e61bcb6488acf5af8d662d5aa702da
Mi-lex <mishin-lex@mail.ru>
2020-01-18 12:40:43 +0300

    make navtoggler more semantically relevant

--- resources/js/components/MainHeader/NavToggler/index.jsx
+++ resources/js/components/MainHeader/NavToggler/index.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { toggleLayout } from '../../../redux/actions';
-import Item from '../Item';
+import classes from '../Item/Item.module.scss';
 
 function NavToggler({ toggleLayout }) {
 	return (
-		<Item href="#" randomProp={'huy'} onClick={ toggleLayout }>
-			<FontAwesomeIcon icon={faBars} />
-		</Item>
+		<li className={classes.item}>
+			<button type="button" className={classes.link} onClick={toggleLayout}>
+				<FontAwesomeIcon icon={faBars} />
+			</button>
+		</li>
 	);
 }
+
+NavToggler.propTypes = {
+	toggleLayout: PropTypes.func.isRequired,
+};
 
 export default connect(null, { toggleLayout })(NavToggler);
